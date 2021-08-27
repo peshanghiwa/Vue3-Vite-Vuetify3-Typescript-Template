aec8169bcff7eefb907830fd5dca7f920143f126
peshanghiwa0 <peshanghiwa0@gmail.com>
2021-08-27 17:32:21 +0300

    added directives

---
+++ src/directives/index.ts
@@ -0,0 +1,13 @@
+import { DirectiveBinding } from "vue";
+
+export default {
+  color: {
+    created(el: HTMLElement, binding: DirectiveBinding): void {
+      if (binding.value && typeof binding.value === "string") {
+        el.style.color = binding.value;
+      } else {
+        el.style.color = "black";
+      }
+    },
+  },
+};
