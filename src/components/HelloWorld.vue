fee611b74c92eb5b24ddf84c031ddd8d75df52c4
peshanghiwa0 <peshanghiwa0@gmail.com>
2021-08-28 23:26:30 +0300

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>{{ msg }}</h1>
   <p class="mb-5">
-    With fully functional configrations for Vuex, SCSS, Vuetify themes,
+    With fully functional configrations for Vuex, Router, SCSS, Vuetify themes,
     Directives, Middlewares, mixins and plugins. <br />
     All written in typescript ES6 Modular form with single file component and
     script setup mode for vue.
