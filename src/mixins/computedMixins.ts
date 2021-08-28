ba97d021fd3f99a36f4a30718f313b8e156de596
peshanghiwa0 <peshanghiwa0@gmail.com>
2021-08-28 22:49:39 +0300

    configured mixin

---
+++ src/mixins/computedMixins.ts
@@ -0,0 +1,7 @@
+exports.colors = {
+  computed: {
+    test() {
+      return "this is a global computed method that is available around the whole project because it is defined as a global mixin";
+    },
+  },
+};
