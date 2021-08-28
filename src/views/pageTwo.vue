c1631ca9a46818c77db404652b3e90a68425f33c
peshanghiwa0 <peshanghiwa0@gmail.com>
2021-08-28 22:49:04 +0300

    made second page

---
+++ src/views/pageTwo.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>This is a protected page</h1>
+  <v-btn class="ma-2" color="#41B883" elevation="0" @click="$router.push('/')"
+    >Go back</v-btn
+  >
+</template>
+<script setup lang="ts"></script>
