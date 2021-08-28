3d0d4f330ebfbcf46990b8a0691582c580e04e84
peshanghiwa0 <peshanghiwa0@gmail.com>
2021-08-28 22:49:29 +0300

    created a protection middleware for second page

---
+++ src/middlewares/protected.ts
@@ -0,0 +1,14 @@
+import { NavigationGuardNext, RouteLocationNormalized } from "vue-router";
+import { store } from "../store";
+
+export default (
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext
+): void => {
+  if (store.getters.getAuth) {
+    next();
+  } else {
+    next("/");
+  }
+};
